1a2d63e5069f8086f741bf4cd38b6d9b6e762405
Bong Nguyen <vanbong@gmail.com>
2021-09-12 12:10:13 +1000

    add more tests for error paths

--- internal/generator/generator_test.go
+++ internal/generator/generator_test.go
@@ -2,6 +2,7 @@ package generator_test
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"testing"
 
@@ -32,7 +33,7 @@ func (w *mockWriter) Write(fileName string, content []byte) error {
 	return err
 }
 
-func Test_Generator(t *testing.T) {
+func Test_Generator_happy(t *testing.T) {
 	loader := &mockSchemaLoader{
 		Schema: &generator.Schema{
 			Tables: map[string]*generator.Table{
@@ -50,4 +51,36 @@ func Test_Generator(t *testing.T) {
 	}
 	require.NoError(t, g.Generate())
 	require.Equal(t, "mock_table.pggo.go\npackage model\n\nimport (\n\t\"github.com/google/uuid\"\n)\n\n// SampleTable represents mock_table table.\ntype SampleTable struct {\n\tID   uuid.UUID\n\tName string\n}\n", writer.String())
+}
+
+func Test_Generator_table_not_found(t *testing.T) {
+	loader := &mockSchemaLoader{
+		Schema: &generator.Schema{
+			Tables: map[string]*generator.Table{
+				"mock_table": {
+					Name: "mock_table",
+				},
+			},
+		},
+	}
+	writer := &mockWriter{}
+	g := &generator.Generator{
+		SchemaLoader: loader,
+		Table:        "mock_table_not_found",
+		Writer:       writer,
+	}
+	require.EqualError(t, g.Generate(), "generator: mock_table_not_found couldn't be found in the schema")
+}
+
+func Test_Generator_schema_error(t *testing.T) {
+	loader := &mockSchemaLoader{
+		Err: errors.New("random error"),
+	}
+	writer := &mockWriter{}
+	g := &generator.Generator{
+		SchemaLoader: loader,
+		Table:        "mock_table",
+		Writer:       writer,
+	}
+	require.EqualError(t, g.Generate(), "random error")
 }
